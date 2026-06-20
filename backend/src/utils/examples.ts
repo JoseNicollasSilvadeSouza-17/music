34c35640acf3917b6cf268e6f0817eb34a1c1976
José Nícollas Silva de Souza <josenicollassilvadesouza@gmail.com>
2026-06-20 17:54:12 -0300

    fix: :pencil2: correct file examples.ts and add more example in caption

--- backend/src/utils/examples.ts
+++ backend/src/utils/examples.ts
@@ -1,7 +1,7 @@
 import type { IMusic } from "../types/IMusic.js";
 import type { CreateMusicDto } from "../types/MusicDTO.js";
 
-const hostExample = "http://file.example.teste/";
+const hostExample = "http://file.example.test";
 
 const exampleSimple: CreateMusicDto = {
 	title: "Disc Zé Do Arrocha",
@@ -14,17 +14,21 @@ const exampleSimple: CreateMusicDto = {
 
 const exampleComplete: IMusic = {
 	...exampleSimple,
-	posterUrl: `${hostExample}/posters/ze-dog`,
-	audioUrl: `${hostExample}/audios/ze-dog`,
-	clipUrl: `${hostExample}/clip/ze-dog`,
+	posterUrl: `${hostExample}/posters/ze-dog.png`,
+	audioUrl: `${hostExample}/audios/ze-dog.mp3`,
+	clipUrl: `${hostExample}/clip/ze-dog.mp4`,
 	caption: {
 		en: "Auuuuuu Arrocha! Let's go, everyone...",
-		es: "¡Auuuuuu Arrocha! ¡Vamos, todos...",
+		"es-ES": "¡Auuuuuu, Arrocha! Vamos allá, gente...",
+		"es-419": "¡Auuuuuu, Arrocha! Vamos, gente...",
+		fr: "Auuuuuu, c'est parti ! Allez, les amis...",
 		gl: "Auuuuuu Arrocha! Imos todos...",
+		it: "Auuuuuu, forza! Andiamo, gente...",
+		la: "Euge! Incipiamus hanc festationem! Agite, omnes...",
 		"pt-BR": "Auuuuuu Arrocha! Simbora minha gente...",
 		"pt-PT": "Auuuuuu Arrocha! Simbora minha gente...",
 	},
-	isoZipUrl: `${hostExample}/iso/zip/ze-dog`,
+	isoZipUrl: `${hostExample}/iso/zip/ze-dog.zip`,
 };
 
 export { exampleSimple, exampleComplete };
